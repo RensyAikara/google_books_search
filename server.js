fb5929dd3a22a8ab5122e68e4bf1f2751a78f6ab
Rensy <rensy.aikara@gmail.com>
2020-06-01 22:08:16 -0500

    test

--- server.js
+++ server.js
@@ -1,10 +1,10 @@
 const express = require("express");
-
+const logger = require("morgan");
 const mongoose = require("mongoose");
 const routes = require("./routes");
 const app = express();
 const PORT = process.env.PORT || 3001;
-
+app.use(logger("dev"));
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
